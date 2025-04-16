2a493e124de4d868a6cb0898031d37cf06aa59be
zfadhel <z.fadhel2004@gmail.com>
2025-04-16 14:45:47 +0300

    first commit

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+
+import main.Calculator;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        calc.divide(5, 0);
+    }
+}
